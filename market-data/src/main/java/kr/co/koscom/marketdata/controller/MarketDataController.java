ffbb97058bca6b28b434d1ed637ca33a9334bee6
교육실 <@192.168.100.136>
2017-04-27 14:41:22 +0900

    404 error

--- market-data/src/main/java/kr/co/koscom/marketdata/controller/MarketDataController.java
+++ market-data/src/main/java/kr/co/koscom/marketdata/controller/MarketDataController.java
@@ -2,7 +2,6 @@ package kr.co.koscom.marketdata.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
