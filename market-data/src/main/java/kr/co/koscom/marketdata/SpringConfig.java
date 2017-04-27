aefb60005c84346671724a87c9525fa4a8bb651a
교육실 <@192.168.100.136>
2017-04-27 14:00:33 +0900

    first push

---
+++ market-data/src/main/java/kr/co/koscom/marketdata/SpringConfig.java
@@ -0,0 +1,10 @@
+package kr.co.koscom.marketdata;
+
+public class SpringConfig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ market-data/src/main/java/kr/co/koscom/marketdata/controller/MarketDataController.java
@@ -0,0 +1,15 @@
+package kr.co.koscom.marketdata.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MarketDataController {
+	@RequestMapping("/MarketData")
+	public void index() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
